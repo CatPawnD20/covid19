99beea6cdf9b0868974a2992100ac1f2924fbb39
CatPawnD20 <ata.osman.ozguz@gmail.com>
2021-04-13 22:57:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+//Logo
+import covidLogo from "./covid-19.svg";
+//Select
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+//Api methods
+import { fetchCountries } from "./Api";
+import { fetchDailyData } from "./Api";
+//Components
+import areaChart from "./components/areaChart";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: "50px auto",
+    width: "50%",
+  },
+}));
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("turkey");
+
+  /*
+    useEffect'in içindeki fonksiyon sadece 1 kere çağılır.
+    */
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      const countries = await fetchCountries();
+      setCountries(countries);
+    };
+
+    fetchCountriesData();
+  }, []);
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container>
+          <img
+            src={covidLogo}
+            alt="Covid19 Logo"
+            style={{
+              height: 100,
+              width: 100,
+              marginTop: 20,
+            }}
+          />
+          <FormControl className={classes.formControl}>
+            <Select
+              value={country}
+              onChange={(event) => setCountry(event.target.value)}
+            >
+              {
+                countries.map(country => (
+                  <MenuItem value={country.Slug}>{country.Country}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+          {/* Chart */}
+          <Grid item xs={12}>
+            
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
